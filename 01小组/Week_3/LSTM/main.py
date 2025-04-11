317b38c6109f4cddde26ad19566b470a05b2dc42
ricckker <3356203629@qq.com>
2025-04-11 10:26:29 +0800

    2025/4/11

--- 01小组/Week_3/LSTM/main.py
+++
@@ -1,8 +0,0 @@
-import pandas as pd
-import matplotlib.pyplot as plt
-plt.rcParams['font.sans-serif'] = 'SimHei' # 设置中文显示
-plt.rcParams['axes.unicode_minus'] = False
-
-df_train = pd.read_csv('delhi_weather.csv', index_col=0, parse_dates=['date'])
-df_test = pd.read_csv('delhi_weather.csv', index_col=0, parse_dates=['date'])
-print(df_train)
